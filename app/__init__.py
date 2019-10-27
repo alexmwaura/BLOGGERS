baec39ef9c1371c643b400578f1d1b83896dfd3b
alexmwaura <alexmwaura43@gmail.com>
2019-10-27 05:57:02 +0300

    add initialization of flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,19 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# from flask_cors import CORS
+
+
+
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+# cors = CORS()
+
 
 
 bootstrap = Bootstrap()
@@ -14,8 +27,13 @@ def create_app(config_name):
 
     app.config.from_object(config_options[config_name])
 
+
+    #initializing flask Extensions
+
+    # cors.init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
