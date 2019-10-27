e9bcae13ddaca4fc0c5a410b52d165e8389f0a99
alexmwaura <alexmwaura43@gmail.com>
2019-10-27 06:05:51 +0300

    Add request functions for requesting Api key and endpoints

--- app/requests.py
+++ app/requests.py
@@ -1,13 +1,16 @@
 import urllib.request,json
 from .models import RandomQuotes
 
+
 api_key = None
 base_url = None
+popular_base_url = None
 
 def configure_request(app):
     global api_key,base_url        
     api_key = app.config["RANDOM_API_KEY"] 
     base_url = app.config["RANDOM_BASE_URL"]
+    popular_base_url = app.config["POPULAR_BASE_URL"]
 
 
 def get_random():
@@ -31,3 +34,23 @@ def get_random():
             random_results = random_results_list
 
     return random_results
+
+# def get_popular_quotes():
+#     '''
+#     Function that gets the json response to our url reques
+#     '''
+    # get_popular_url = popular_base_url
+    # with urllib.request.urlopen(get_popular_url) as url:
+
+    #     get_popular_data = url.read()
+    #     get_popular_response = json.loads(get_popular_data)
+
+    #     popular_results = None
+
+    #     if get_popular_response:
+
+    #         popular_results_list = get_popular_response
+
+    # return popular_results_list
+
+
