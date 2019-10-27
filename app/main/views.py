448850f67733788a5f1d3cd0a9df0929d527ab48
alexmwaura <alexmwaura43@gmail.com>
2019-10-27 22:50:52 +0300

    Add function and route for new comments also add Subscriber from models and condition for subscriber in new blogs function

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,13 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template,request,redirect,url_for,abort,flash
 from .import main
 from ..requests import get_random
-from ..models import RandomQuotes,Blogs,Comments
+from ..models import RandomQuotes,Blogs,Comments,Subscriber
+from flask_login import login_required, current_user
 
 from .forms import BlogForm,CommentForm
 
 @main.route("/")
+
 def index():
     '''
 
@@ -14,19 +16,21 @@ def index():
     '''
 
     quote = get_random()
-    blogs = Blogs.query.all()
-    
+    blogs = Blogs.query.order_by(Blogs.date_posted.desc())
+
+    comment = Comments.query.filter_by().all()
     title = "Welcome to the best blogging site"
 
-    return render_template("index.html",title = title,quote = quote, blogs=blogs)
+    return render_template("index.html",title = title,quote = quote, blogs=blogs,comment = comment)
 
 
 @main.route("/blogs",methods = ["GET","POST"])
+@login_required
 def new_blogs():
     '''
     View root function that returns blogs
     '''
-
+    subscriber = Subscriber.query.all()
     blog_forms = BlogForm()
     
     
@@ -36,22 +40,40 @@ def new_blogs():
 
         title = blog_forms.title.data
         blogs = blog_forms.blogs.data
-        new_blogs = Blogs(title = title,blogs = blogs) 
+        user_id = current_user._get_current_object().id
+        new_blogs = Blogs(title = title,blogs = blogs,user_id = user_id) 
 
         new_blogs.save_blog()
+        for subscriber in subscriber:
+            flash("You posted a new blog")
+
         
 
         return redirect(url_for("main.index",blogs = blogs,title = title))
         
-       
+        comment = Comments.get_comments()
         
        
 
     return render_template('blog.html',blog_forms = blog_forms)
 
-# @main.route("/blog/comment/<int:id>")
-# def new_commnet(id):
-#     comment_form = CommentForm()
+    
+
+@main.route("/blog/comment",methods = ["GET","POST"])
+@login_required
+def new_comment():
+    comment_form = CommentForm()
+    blogs = Blogs.query.all()
+    
+
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data
+        new_comment = Comments(comment = comment,user_id = current_user._get_current_object().id)
+        new_comment.save_comment()
+
+        return redirect(url_for("main.index",comment = comment))
+
+    return render_template("comment.html",comment_form = comment_form)    
 
 
 
