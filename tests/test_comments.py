b98f2170a59de594f4e55da7c376b9ebc500d61d
alexmwaura <alexmwaura43@gmail.com>
2019-10-28 22:41:02 +0300

    Add test files

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,14 +1,14 @@
 import unittest
-from app.models import Comment, Blog, User
+from app.models import Comments, Blogs, User
 from app import db
 
 class CommentModelTest(unittest.TestCase):
     def setUp(self):  
         
-        self.new_comment = Comment(id = 1, comment = 'Test comment', user = self.user_emma, blog_id = self.new_blog)
+        self.new_comment = Comments(id = id, comment = 'Test comment', users_id = User)
         
     def tearDown(self):
-        Blog.query.delete()
+        Blogs.query.delete()
         User.query.delete()
     
     def test_check_instance_variables(self):
@@ -20,24 +20,24 @@ class CommentModelTest(unittest.TestCase):
 class CommentModelTest(unittest.TestCase):
     def setUp(self):
         self.user_charles = User(username='cha', password='chako', email='test@test.com')
-        self.new_blog = Blog(id=1, title='Test', content='This is a test blog', user_id=self.user_charles.id)
-        self.new_comment = Comment(id=1, comment ='This is a test comment', user_id=self.user_charles.id, blog_id = self.new_blog.id )
+        self.new_blog = Blogs(id=1, title='Test', blogs='This is a test blog', user_id=self.users.id)
+        self.new_comment = Comments(id=1, comment ='This is a test comment', user_id=self.user.id, blogs_id = self.new_blog.id )
 
     def tearDown(self):
-        Blog.query.delete()
+        Blogs.query.delete()
         User.query.delete()
-        Comment.query.delete()
+        Comments.query.delete()
 
     def test_check_instance_variables(self):
         self.assertEquals(self.new_comment.comment, 'This is a test comment')
-        self.assertEquals(self.new_comment.user_id, self.user_charles.id)
-        self.assertEquals(self.new_comment.blog_id, self.new_blog.id)
+        self.assertEquals(self.new_comment.users_id, self.user_charles.id)
+        self.assertEquals(self.new_comment.blogs_id, self.new_blog.id)
 
     def test_save_comment(self):
         self.new_comment.save()
-        self.assertTrue(len(Comment.query.all()) > 0)
+        self.assertTrue(len(Comments.query.all()) > 0)
 
     def test_get_comment(self):
         self.new_comment.save()
-        get_comment = Comment.get_comment(1)
+        get_comment = Comments.get_comment(1)
         self.assertTrue(get_comment is not None)
