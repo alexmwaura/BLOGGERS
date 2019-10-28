13eedc9b76d97f3bd558707b7fdc855e2f1f7a8e
alexmwaura <alexmwaura43@gmail.com>
2019-10-28 15:12:43 +0300

    initialize mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 # from flask_cors import CORS
 
@@ -20,8 +21,8 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
-
+mail = Mail()
+simplemde = SimpleMDE
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -35,7 +36,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
+    mail.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
