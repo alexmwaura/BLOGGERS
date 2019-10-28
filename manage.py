86433feac5b126c0e2ce46287696409911a5ec2f
alexmwaura <alexmwaura43@gmail.com>
2019-10-28 22:07:47 +0300

    push to heroku

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Comments,Blogs
 
-app = create_app('production')
+app = create_app('Production')
 manager = Manager(app)
 
 migrate = Migrate(app,db)
