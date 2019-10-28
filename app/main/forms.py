3e48f05dfa19e4be943740ae4a5a4bd84dd49600
alexmwaura <alexmwaura43@gmail.com>
2019-10-28 15:17:31 +0300

    Add flask form for subscribers and import Email,Equal to from wtfforms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
-
+from wtforms.validators import Required,Email,EqualTo
+from ..models import Subscriber
+from wtforms import ValidationError
 
 class BlogForm(FlaskForm):
     title = StringField('Blog title',validators = [Required()])
     blogs = StringField('Your Blog',validators = [Required()])
     submit = SubmitField('Submit')
+
+class SubscriberForm(FlaskForm):
+    email = StringField("Email Address",validators=[Required(),Email()])
+    submit = SubmitField("Subscribe")
+
+    def validate_email(self,data_field):
+        if Subscriber.query.filter_by(email =data_field.data).first():
+            raise ValidationError("Account already subscribed with that email")
 
 
 class CommentForm(FlaskForm):
