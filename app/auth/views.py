91d0feade06db7e8adaa08d36b2edfd23cba36e0
alexmwaura <alexmwaura43@gmail.com>
2019-10-29 13:59:34 +0300

    update heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,7 +34,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Pitches login"
+    title = "BLogs login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
